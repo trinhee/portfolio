a2e7d6f3014b0d58ead5d6569701b1f2d21ca508
Ethan <ethan.trinh4@gmail.com>
2025-01-19 03:15:00 -0500

    added logo

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,17 +1,20 @@
 import './Nav.css'
+import IconLogo from './logo.tsx'
 
 function Nav() {
   return (
     
     <header className="header">
-        <a href="/" className="logo">E</a>
+        <a href="/" className="logo">
+        <IconLogo/>
+        </a>
 
         <nav className="navbar">
           <a href="/">About</a>
           <a href="/">Experience</a>
           <a href="/">Work</a>
           <a href="/">Contact</a>
-          <a href="/" id='resume'>Resume</a>
+          <a href="https://docs.google.com/document/d/1RMDgc0sYO-VZ823LUVCbRsq7WWdMIvKz2-h5Cxn1lGI/edit?usp=drive_link" target="_blank" id='resume'>Resume</a>
 
         </nav>
     </header>    
---
+++ src/logo.tsx
@@ -0,0 +1,23 @@
+const IconLogo = () => (
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
+    <title>Logo</title>
+    <g transform="translate(-8.000000, -2.000000)">
+      <g transform="translate(11.000000, 5.000000)">
+        <polygon
+          id="Shape"
+          stroke="currentColor"
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          points="39 0 0 22 0 67 39 90 78 68 78 23"
+        />
+        <path
+          d="M32 30 L50 30 L50 34 L36 34 L36 43 L50 43 L50 47 L36 47 L36 56 L50 56 L50 60 L32 60 L32 30 Z"
+          fill="currentColor"
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+export default IconLogo;
