4c452cd561305aaaefab704b458771ebb9fc3567
Ethan <ethan.trinh4@gmail.com>
2025-01-19 04:20:32 -0500

    styling

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -3,7 +3,7 @@ import './Hero.css'
 function Hero(){
     return(
         <body>
-        <div className="hero">
+        <div className="hero" id='hero'>
             <p className="intro">Hi, my name is</p>
             <h1 className='name'>Ethan Trinh.</h1>
             <h2 className='tagline'>I build things.</h2>
@@ -11,7 +11,7 @@ function Hero(){
               I'm a software developer, computer science student, and innovator. I also have a love for cats!
             </p>
 
-            <a href="https://docs.google.com/presentation/d/1V-RjEP-uA9VNTSAPscU4xPDbIRitUe7VRgzZmBwhuwg/edit?usp=sharing" id="kawi" target='_blank'>Meet Kawi!</a>
+            <a href="https://docs.google.com/presentation/d/1V-RjEP-uA9VNTSAPscU4xPDbIRitUe7VRgzZmBwhuwg/edit?usp=sharing" id="kawi" target='_blank'>Meet my pal Kawi !</a>
         </div>
     </body>
     );
--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,16 +1,49 @@
+import React, { useState, useEffect } from 'react'
 import './Nav.css'
 import IconLogo from './logo.tsx'
 
 function Nav() {
+  const [isScrollingDown, setIsScrollingDown] = useState(false);
+  const [isShadowVisible, setIsShadowVisible] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      // Detect scrolling direction
+      if (currentScrollY > lastScrollY) {
+        setIsScrollingDown(true); // Scrolling down
+      } else {
+        setIsScrollingDown(false); // Scrolling up
+      }
+
+      // Add shadow if the navbar is not at the top
+      if (currentScrollY > 10) {
+        setIsShadowVisible(true);
+      } else {
+        setIsShadowVisible(false);
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [lastScrollY]);
   return (
     
-    <header className="header">
-        <a href="/" className="logo">
+    <header className= {`header ${isScrollingDown ? 'hide' : ''} ${
+      isShadowVisible ? 'shadow' : ''
+      }`}>
+        <a href="#hero" className="logo">
         <IconLogo/>
         </a>
 
         <nav className="navbar">
-          <a href="/">About</a>
+          <a href="#about">About</a>
           <a href="/">Experience</a>
           <a href="/">Work</a>
           <a href="/">Contact</a>
